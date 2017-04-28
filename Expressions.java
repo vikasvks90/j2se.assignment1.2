3f15d353ae9772fe9e270dac5b584565495b55aa
vikasvks90 <vikas.varshney90@gmail.com>
2017-04-28 18:47:44 +0530

    Add files via upload

---
+++ Expressions.java
@@ -0,0 +1,21 @@
+package Assignment1;
+
+public class Expressions {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 10;
+		int c,d,e;
+		b= a-- - --a;
+		c=a--;
+		d=a>>2;
+		e=a&b;
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+		System.out.println("c: "+c);
+		System.out.println("d: "+d);
+		System.out.println("e: "+e);
+
+	}
+
+}
